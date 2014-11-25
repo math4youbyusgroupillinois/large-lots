ad9d08853a3aa5ee3b4185c7db1ef33b418c2f6b
Derek Eder <derek.eder@gmail.com>
2014-11-25 14:44:57 -0600

    updated contact info to Michael, changed pilot to large lots, changed spreadsheet output to uppercase address

--- lots_admin/views.py
+++ lots_admin/views.py
@@ -58,107 +58,75 @@ def csv_dump(request, pilot):
     response['Content-Disposition'] = 'attachment; filename=Large_Lots_Applications_%s_%s.csv' % (pilot, now)
     applications = Application.objects.filter(pilot=pilot)
     header = [
-        'ID', 
-        'Date received', 
-        'Name', 
-        'Organization', 
-        'Owned Full Address', 
-        'Owned Street Number', 
-        'Owned Street Direction',
-        'Owned Street Name',
-        'Owned Street Type',
-        'Owned PIN', 
+        'ID',
+        'Date received',
+        'First Name',
+        'Last Name',
+        'Organization',
+        'Owned Address',
+        'Owned Full Address',
+        'Owned PIN',
         'Deed Image URL',
         'Contact Address',
-        'Phone', 
-        'Email', 
+        'Phone',
+        'Email',
         'Received assistance',
         'Lot 1 PIN',
+        'Lot 1 Address',
         'Lot 1 Full Address',
-        'Lot 1 Street Number',
-        'Lot 1 Street Direction',
-        'Lot 1 Street Name',
-        'Lot 1 Street Type',
         'Lot 1 Image URL',
         'Lot 2 PIN',
+        'Lot 2 Address',
         'Lot 2 Full Address',
-        'Lot 2 Street Number',
-        'Lot 2 Street Direction',
-        'Lot 2 Street Name',
-        'Lot 2 Street Type',
         'Lot 2 Image URL',
     ]
     rows = []
     for application in applications:
-        owned_full_address = '%s %s %s %s' % \
+        owned_address = '%s %s %s %s' % \
             (getattr(application.owned_address, 'street', ''),
-             getattr(application.owned_address, 'city', ''),
-             getattr(application.owned_address, 'state', ''),
-             getattr(application.owned_address, 'zip_code', ''))
-        owned_street_number = application.owned_address.street_number
-        owned_street_dir = application.owned_address.street_dir
-        owned_street_name = application.owned_address.street_name
-        owned_street_type = application.owned_address.street_type
+            getattr(application.owned_address, 'city', ''),
+            getattr(application.owned_address, 'state', ''),
+            getattr(application.owned_address, 'zip_code', ''))
         contact_address = '%s %s %s %s' % \
             (getattr(application.contact_address, 'street', ''),
-             getattr(application.contact_address, 'city', ''),
-             getattr(application.contact_address, 'state', ''),
-             getattr(application.contact_address, 'zip_code', ''))
+            getattr(application.contact_address, 'city', ''),
+            getattr(application.contact_address, 'state', ''),
+            getattr(application.contact_address, 'zip_code', ''))
         lots = []
         for lot in application.lot_set.all():
-            full_addr = '%s %s %s %s' % \
+            addr = getattr(lot.address, 'street', '').upper()
+            addr_full = '%s %s %s %s' % \
                 (getattr(lot.address, 'street', ''),
-                 getattr(lot.address, 'city', ''),
-                 getattr(lot.address, 'state', ''),
-                 getattr(lot.address, 'zip_code', ''))
+                getattr(lot.address, 'city', ''),
+                getattr(lot.address, 'state', ''),
+                getattr(lot.address, 'zip_code', ''))
             pin = lot.pin
             image_url = 'http://cookviewer1.cookcountyil.gov/Jsviewer/image_viewer/requestImg.aspx?%s=' % pin.replace('-', '')
-            lots.extend([
-                pin, 
-                full_addr, 
-                lot.address.street_number, 
-                lot.address.street_dir, 
-                lot.address.street_name,
-                lot.address.street_type,
-                image_url
-            ])
-        if len(lots) < 8:
-            lots.extend(['', '', '', '', '', '', ''])
-        lot_1 = lots[:7]
-        lot_2 = lots[7:]
-        lot_1_pin, lot_1_addr, lot_1_street_number, lot_1_street_dir, \
-            lot_1_street_name, lot_1_street_type, lot_1_image = lot_1
-        lot_2_pin, lot_2_addr, lot_2_street_number, lot_2_street_dir, \
-            lot_2_street_name, lot_2_street_type, lot_2_image = lot_2
+            lots.extend([pin, addr, addr_full, image_url])
+        if len(lots) < 4:
+            lots.extend(['', '', ''])
+        lot_1_pin, lot_1_addr, lot_1_addr_full, lot_1_image, lot_2_pin, lot_2_addr, lot_2_addr_full, lot_2_image = lots
         rows.append([
             application.id,
             application.received_date.strftime('%Y-%m-%d %H:%m %p'),
-            '%s %s' % (application.first_name, application.last_name),
+            application.first_name,
+            application.last_name,
             application.organization,
-            owned_full_address, 
-            owned_street_number, 
-            owned_street_dir, 
-            owned_street_name, 
-            owned_street_type, 
+            getattr(application.owned_address, 'street', '').upper(),
+            owned_address,
             application.owned_pin,
             application.deed_image.url,
-            contact_address, 
+            contact_address,
             application.phone,
             application.email,
             application.how_heard,
             lot_1_pin,
             lot_1_addr,
-            lot_1_street_number,
-            lot_1_street_dir,
-            lot_1_street_name,
-            lot_1_street_type,
+            lot_1_addr_full,
             lot_1_image,
             lot_2_pin,
             lot_2_addr,
-            lot_2_street_number,
-            lot_2_street_dir,
-            lot_2_street_name,
-            lot_2_street_type,
+            lot_2_addr_full,
             lot_2_image,
         ])
     writer = csv.writer(response)
